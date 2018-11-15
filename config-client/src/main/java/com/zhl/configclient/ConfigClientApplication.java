444fe0f37586c5917e2f4b23d9b64bfdabd74375
zhanghailong <zhanghailong@bitnei.cn>
2018-11-15 13:27:29 +0800

    spring cloud 配置中心的搭建测试

---
+++ config-client/src/main/java/com/zhl/configclient/ConfigClientApplication.java
@@ -0,0 +1,12 @@
+package com.zhl.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
---
+++ config-client/src/main/java/com/zhl/configclient/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.zhl.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @Value("${foo}")
+    String foo;
+
+    @RequestMapping(value = "/hi")
+    public String index(){
+        return foo;
+    }
+}
